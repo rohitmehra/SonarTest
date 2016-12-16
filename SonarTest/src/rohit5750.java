e6719f52ebc81c296f36d60e62c7f16f5f27ecdd
Rohit5750 <rohit5750@yahoo.com>
2016-12-16 16:46:25 +0530

    Commit Rohit5750 1

---
+++ SonarTest/src/rohit5750.java
@@ -0,0 +1,39 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class rohit5750 {
+	private static int count = 0;
+	Calendar c = new GregorianCalendar(2014, 12, 25);
+	int y;
+
+	public void dob() {
+		int x;
+		if (c.get(Calendar.MONTH) == 12)
+			x = 1;
+	}
+
+	public void setname() {
+
+	}
+
+	public boolean check(boolean a) {
+		return a;
+	}
+
+	public void numberOfStudent() {
+		if (2 < 3) {
+			setname();
+		}
+		if (check(true) | check(false))
+			count++;
+
+		try {
+			y = count / 0;
+			throw new IllegalArgumentException();
+		} finally {
+
+			throw new RuntimeException();
+		}
+
+	}
+}
